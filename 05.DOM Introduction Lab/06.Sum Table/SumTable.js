2e7650ac4bca05b3c3feb6f456fb0e88bbe1e79d
Stefan Sabev <stevenbg91@gmail.com>
2023-11-26 17:50:58 +0100

    Dom lab

--- 05.DOM Introduction Lab/06.Sum Table/SumTable.js
+++ 05.DOM Introduction Lab/06.Sum Table/SumTable.js
@@ -1,12 +1,13 @@
 function sumTable() {
- let rows = document.querySelectorAll("td");
+  
+   let rows = document.querySelectorAll("table tr");
+   let totalSum = 0;
 
- let sum = 0;
- for (let index = 1; index < rows; index+= 2) {
-    
-    let currentNumber = Number(rows[index]);
-    sum += currentNumber;
- }
- console.log(sum)
- console.log("hello")
+   for (let index = 1; index < rows.length; index++) {
+      
+      let cells = rows[index].children;
+      let cellPrice = Number(cells[1].textContent);
+      totalSum += cellPrice;
+   }
+   document.getElementById("sum").textContent = totalSum;
 }
--- 05.DOM Introduction Lab/07.Extract Parenthesis/ExtractParenthesis.js
+++ 05.DOM Introduction Lab/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,20 @@
 function extract(content) {
 
+    let text = document.getElementById(content).textContent;
+    const pattern = /\(([^)]+)\)/g;
+
+    let matches = text.match(pattern);
+    let wordsArr = [];
+
+    if (matches) {
+        matches.forEach(match => {
+            let words = match.match(/\b\w+\b/g);
+            if (words) {
+                let concatenatedString = words.join(' ');
+                wordsArr.push(concatenatedString);
+            }
+        });
+    }
+
+    return wordsArr.join("; ")
 }
