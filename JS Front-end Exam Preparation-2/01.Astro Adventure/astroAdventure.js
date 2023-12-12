63b0ce40ac0c54c457074e02547045cba5fcc0ec
Stefan Sabev <stevenbg91@gmail.com>
2023-12-12 18:56:32 +0100

    JS front end exam preparation 1

--- JS Front-end Exam Preparation-2/01.Astro Adventure/astroAdventure.js
+++ JS Front-end Exam Preparation-2/01.Astro Adventure/astroAdventure.js
@@ -1,1 +1,87 @@
-//TODO...
+function solve(array) {
+
+    let numberOfAustronafts = Number(array.shift());
+    let team = [];
+
+    for (let index = 0; index < numberOfAustronafts; index++) {
+        const austronaftInfo = array.shift().split(" ");
+        let obj = {
+            name: austronaftInfo[0],
+            oxygen: Number(austronaftInfo[1]),
+            energy: Number(austronaftInfo[2])
+        }
+        
+        team.push(obj);
+
+    }
+    for (const item of array) {
+        
+    }
+
+    let line = array.shift().split(" - ");
+    let command = line[0];
+
+    while (command !== "End") {
+
+        let austronaftName = line[1];
+        let austronaft = team.find(x => x.name === austronaftName);
+
+        switch (command) {
+
+
+            case "Explore":
+                let energy = Number(line[2]);
+                if(austronaft.energy >= energy){
+                    austronaft.energy -= energy;
+                    console.log(`${austronaft.name} has successfully explored a new area and now has ${austronaft.energy} energy!`);
+                }else{
+                    console.log(`${austronaft.name} does not have enough energy to explore!`);
+                }
+                break;
+            case "Refuel":
+                let amount = Number(line[2]);
+                let oldEnergy = austronaft.energy;
+                austronaft.energy += amount;
+                if(austronaft.energy > 200){
+                    austronaft.energy = 200;
+                }
+                let recovered = austronaft.energy - oldEnergy;
+                console.log(`${austronaft.name} refueled their energy by ${recovered}!`);
+
+
+                break;
+            case "Breathe":
+                let amountOfOxigen = Number(line[2]);
+                let oldOxigen = austronaft.oxygen;
+                austronaft.oxygen += amountOfOxigen;
+                if(austronaft.oxygen > 100){
+                    austronaft.oxygen = 100;
+                }
+                let recoveredOxigen = austronaft.oxygen - oldOxigen;
+                console.log(`${austronaft.name} took a breath and recovered ${recoveredOxigen} oxygen!`);
+
+                break;
+        }
+
+
+
+        line = array.shift().split(" - ");
+        command = line[0];
+    }
+
+
+    for (const austronaft of team) {
+        console.log(`Astronaut: ${austronaft.name}, Oxygen: ${austronaft.oxygen}, Energy: ${austronaft.energy}`);
+    }
+
+}
+
+solve(['3',
+    'John 50 120',
+    'Kate 80 180',
+    'Rob 70 150',
+    'Explore - John - 50',
+    'Refuel - Kate - 30',
+    'Breathe - Rob - 20',
+    'End']
+);
--- JS Front-end Exam Preparation-2/02. Dart Scoring/app.js
+++ JS Front-end Exam Preparation-2/02. Dart Scoring/app.js
@@ -1,6 +1,88 @@
 window.addEventListener("load", solve);
 
 function solve() {
-    //TODO
-  }
-  
+
+    let addBtn = document.getElementById("add-btn");
+    let playerNameField = document.getElementById("player");
+    let scoreField = document.getElementById("score");
+    let roundField = document.getElementById("round");
+
+    let ul = document.getElementById("sure-list");
+
+
+    addBtn.addEventListener('click', addEvent);
+
+
+
+    function addEvent(e) {
+        e.preventDefault();
+
+        let playerName = playerNameField.value;
+        let score = scoreField.value;
+        let round = roundField.value;
+
+        let li = document.createElement("li");
+        li.className = "dart-item";
+        let article = document.createElement("article");
+        let p1 = document.createElement("p");
+        let p2 = document.createElement("p");
+        let p3 = document.createElement("p");
+        p1.textContent = playerName;
+        p2.textContent = `Score: ${score}`;
+        p3.textContent = `Round: ${round}`;
+        let btnEdit = document.createElement("button");
+        btnEdit.textContent = "edit";
+        btnEdit.className = "btn edit"
+
+        let btnOk = document.createElement("button");
+        btnOk.className = "btn ok";
+        btnOk.textContent = "ok";
+        article.appendChild(p1);
+        article.appendChild(p2);
+        article.appendChild(p3);
+        li.appendChild(article);
+        li.appendChild(btnEdit);
+        li.appendChild(btnOk);
+        ul.appendChild(li);
+
+        playerNameField.value = "";
+        scoreField.value = "";
+        roundField.value = "";
+
+        addBtn.disabled = true
+
+        btnEdit.addEventListener('click', editForm);
+        btnOk.addEventListener('click',addToScoreBoard);
+
+        function editForm(e) {
+            e.preventDefault();
+
+            li.remove();
+            addBtn.disabled = false;
+            playerNameField.value = playerName;
+            scoreField.value = score;
+            roundField.value = round;
+        }
+
+        function addToScoreBoard(e){
+            e.preventDefault();
+            let ulScoreBoard = document.getElementById("scoreboard-list");
+            li.removeChild(btnEdit);
+            li.removeChild(btnOk);
+            ul.removeChild(li);
+            ulScoreBoard.appendChild(li);
+            addBtn.disabled = false;
+
+        }
+        document.getElementsByClassName("btn clear")[0].addEventListener('click', clearScoreBoard);
+
+        function clearScoreBoard(e){
+            e.preventDefault();
+
+            console.log("hello from clear");
+            window.location.reload();
+        }
+
+    }
+
+}
